78f3fd031d7f81deba730a9258c72171dc8ed5d7
PoushnayaA <poushnaya@bk.ru>
2024-06-15 13:11:40 +0300

    add sort list

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,6 @@
-import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
-import {Form} from './modules/form-validate/form';
+import { iosVhFix } from './utils/ios-vh-fix';
+import { initModals } from './modules/modals/init-modals';
+import { Form } from './modules/form-validate/form';
 
 // ---------------------------------
 
@@ -31,17 +31,43 @@ window.addEventListener('DOMContentLoaded', () => {
     document.querySelector('body').classList.toggle('dark');
   });
 
+  const bodyElement = document.body;
+
   function handleWindowResize() {
-    const bodyElement = document.body;
     const windowWidth = window.innerWidth;
 
     if (windowWidth >= 768) {
       bodyElement.classList.remove('dark');
       menu.classList.remove('show');
-    menu.classList.add('hide');
-    burger.classList.remove('active');
+      menu.classList.add('hide');
+      burger.classList.remove('active');
     }
   }
 
   window.addEventListener('resize', handleWindowResize);
+
+  const filtersToggle = document.querySelector('.competitions-list__filter-mobile-button');
+  const filtersContainer = document.querySelector('.filters');
+
+  filtersToggle.addEventListener('click', () => {
+    filtersContainer.classList.toggle('active');
+    document.querySelector('body').classList.toggle('dark');
+  });
+
+  const sortToggle = document.querySelector('.competitions-list__sort-button');
+  const sortOptions = document.querySelector('.competitions-list__sort-list');
+
+  sortToggle.addEventListener('click', () => {
+    sortOptions.classList.toggle('active');
+  });
+
+  const sortLinks = sortOptions.querySelectorAll('a');
+  sortLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const sortBy = event.target.dataset.sort;
+      console.log(`Sorting by ${sortBy}`);
+      sortOptions.style.display = 'none';
+    });
+  });
 });
