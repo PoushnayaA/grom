a54b02a3b0e83d3670c4b6b749a5bc8045eb075a
PoushnayaA <poushnaya@bk.ru>
2024-06-19 02:41:23 +0300

    edit filters

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,10 @@
+import IMask from 'imask';
 import { iosVhFix } from './utils/ios-vh-fix';
 import { initModals } from './modules/modals/init-modals';
 import { Form } from './modules/form-validate/form';
+
+
+import { initAccordions } from './modules/accordion/init-accordion';
 
 // ---------------------------------
 
@@ -19,7 +23,7 @@ window.addEventListener('DOMContentLoaded', () => {
     form.init();
   });
 
-
+  setTimeout(initAccordions(), 1000);
 
   const burger = document.querySelector('.burger');
   const menu = document.querySelector('.menu');
@@ -51,7 +55,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
   filtersToggle.addEventListener('click', () => {
     filtersContainer.classList.toggle('active');
-    document.querySelector('body').classList.toggle('dark');
+    document.querySelector('body').classList.toggle('dark-modal');
+  });
+
+  window.addEventListener('resize', () => {
+
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth >= 768) {
+      filtersContainer.classList.remove('active');
+      document.querySelector('body').classList.remove('dark-modal');
+    }
   });
 
   const sortToggle = document.querySelector('.competitions-list__sort-button');
@@ -67,24 +81,163 @@ window.addEventListener('DOMContentLoaded', () => {
     link.addEventListener('click', (event) => {
       event.preventDefault();
       const sortBy = event.target.dataset.sort;
-      console.log(`Sorting by ${sortBy}`);
       sortOptions.style.display = 'none';
     });
   });
 
   const addressElements = document.querySelectorAll('.competitions__address');
 
-function updateAddresses() {
-  const windowWidth = window.innerWidth;
+  function updateAddresses() {
+    const windowWidth = window.innerWidth;
 
-  addressElements.forEach(addressElement => {
-    const shortAddress = addressElement.dataset.addressMobile;
-    const longAddress = addressElement.dataset.addressDesktop;
-    addressElement.textContent = windowWidth >= 1440 ? longAddress : shortAddress;
+    addressElements.forEach(addressElement => {
+      const shortAddress = addressElement.dataset.addressMobile;
+      const longAddress = addressElement.dataset.addressDesktop;
+      addressElement.textContent = windowWidth >= 1440 ? longAddress : shortAddress;
+    });
+  }
+
+  updateAddresses();
+  window.addEventListener('resize', updateAddresses);
+
+  const competitionsButtons = document.querySelectorAll('.competitions__buttons');
+  competitionsButtons.forEach(button => {
+    const buttonRegistration = button.querySelector('a').getAttribute('data-button-registration');
+    const priceElement = button.querySelector('p');
+    if (buttonRegistration == 'over' || buttonRegistration == 'registration-completed') {
+      priceElement.style.display = 'none';
+    } else if (buttonRegistration == 'registration-active') {
+      priceElement.style.display = 'block';
+    }
   });
-  console.log(1);
-}
 
-updateAddresses();
-window.addEventListener('resize', updateAddresses);
+  const filterInput = document.querySelector('.filter-input');
+  const filterListSearch = document.getElementById('filter-category').querySelectorAll('.filter-item');
+  const filterList = document.querySelectorAll('.filter-item');
+  const selectedContainer = document.querySelector('.selected-items');
+
+  filterInput.addEventListener('input', () => {
+    const searchTerm = filterInput.value.toLowerCase();
+    filterListSearch.forEach(item => {
+      const text = item.textContent.toLowerCase();
+      if (text.includes(searchTerm)) {
+        item.classList.remove('hidden');
+      } else {
+        item.classList.add('hidden');
+      }
+    });
+  });
+
+  filterList.forEach(item => {
+    const checkbox = item.querySelector('input[type="checkbox"]');
+    checkbox.addEventListener('change', () => {
+      const text = item.textContent;
+      if (checkbox.checked) {
+        addToSelectedContainer(text, item);
+      } else {
+        removeFromSelectedContainer(text, item);
+      }
+    });
+  });
+
+  function addToSelectedContainer(text, item) {
+    selectedContainer.classList.remove('visually-hidden');
+    const selectedItem = document.createElement('div');
+    selectedItem.classList.add('selected-item');
+    selectedItem.dataset.text = text; // Сохраняем текст в data-атрибуте
+
+    const selectedItemText = document.createElement('span');
+    selectedItemText.textContent = text;
+
+    const removeBtn = document.createElement('span');
+    removeBtn.classList.add('remove-btn');
+
+    // Создаем SVG элемент
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+    svg.setAttribute('width', '10');
+    svg.setAttribute('height', '10');
+    svg.setAttribute('viewBox', '0 0 10 10');
+    svg.setAttribute('fill', 'none');
+
+    // Создаем SVG path элемент
+    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttribute('d', 'M1 1L9 9M9 1L1 9');
+    path.setAttribute('stroke', '#999999');
+    svg.appendChild(path);
+    removeBtn.appendChild(svg);
+
+    removeBtn.addEventListener('click', () => {
+      removeFromSelectedContainer(text, item);
+    });
+
+    selectedItem.appendChild(selectedItemText);
+    selectedItem.appendChild(removeBtn);
+    selectedContainer.appendChild(selectedItem);
+  }
+
+  function removeFromSelectedContainer(text, item) {
+    const selectedItems = selectedContainer.querySelectorAll('.selected-item');
+    selectedItems.forEach(selectedItem => {
+      if (selectedItem.dataset.text === text) {
+        selectedItem.remove();
+        const checkbox = item.querySelector('input[type="checkbox"]');
+        checkbox.checked = false;
+
+        if (document.querySelectorAll('.selected-item').length === 0) {
+          selectedContainer.classList.add('visually-hidden');
+        }
+      }
+    });
+
+
+  }
+
+
+
+
+
+
+  const dateInputField = document.querySelector('.date-input__field');
+  const dateInputBtn = document.querySelector('.date-input__btn');
+
+
+  new Cleave(dateInputField, {
+    date: true,
+    datePattern: ['d', 'm', 'Y'],
+    delimiter: '.',
+    blocks: [2, 2, 4],
+    maxLength: 10,
+
+  });
+
+
+
+dateInputBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  dateInputField.showPicker();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+    const form = document.getElementById('filterForm');
+
+    form.addEventListener('submit', (event) => {
+      // event.preventDefault();
+    });
+
+    form.querySelector('button').addEventListener('click', (event) => {
+      event.preventDefault();
+    })
+
 });
